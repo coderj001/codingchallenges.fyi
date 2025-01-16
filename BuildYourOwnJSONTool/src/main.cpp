7a279e19c93d3b906f69a348da6dc2afe5975089
Raju Ghorai <rajughorai41410@gmail.com>
2025-01-16 18:49:54 +0530

    added: token code

--- BuildYourOwnJSONTool/src/main.cpp
+++ BuildYourOwnJSONTool/src/main.cpp
@@ -1,5 +1,34 @@
+#include <bits/stdc++.h>
+#include <cstddef>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-}
+enum TokenType {
+  LEFT_BRACE = '{',
+  RIGHT_BRACE = '}',
+  LEFT_BRACKET = '(',
+  RIGHT_BRACKET = ')',
+  LEFT_SQUARE_BRACKET = '[',
+  RIGHT_SQUARE_BRACKET = ']',
+  COMMA = ',',
+  COLON = ';',
+  DOUBLE_QUOTE = '"',
+  BOOLEAN,
+  NUMBER,
+  STRING,
+  NULL_VALUE
+};
+
+struct Token {
+  TokenType type;
+  string value;
+  size_t line;
+  size_t colunm;
+
+  Token(TokenType t, const string &v, size_t l = 0, size_t c = 0)
+      : type(t), value(v), line(l), colunm(c) {}
+};
+
+int main(int argc, char *argv[]) { return 0; }
