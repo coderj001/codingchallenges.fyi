f6daeee242eb467e4ccd6555aa97bca244eeb6ad
Raju Ghorai <rajughorai41410@gmail.com>
2025-01-16 19:44:54 +0530

    update: token str

--- BuildYourOwnJSONTool/src/main.cpp
+++ BuildYourOwnJSONTool/src/main.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
+#include <cctype>
 #include <cstddef>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 using namespace std;
@@ -13,7 +15,8 @@ enum TokenType {
   LEFT_SQUARE_BRACKET = '[',
   RIGHT_SQUARE_BRACKET = ']',
   COMMA = ',',
-  COLON = ';',
+  COLON = ':',
+  SEMICOLON = ';',
   DOUBLE_QUOTE = '"',
   BOOLEAN,
   NUMBER,
@@ -29,6 +32,82 @@ struct Token {
 
   Token(TokenType t, const string &v, size_t l = 0, size_t c = 0)
       : type(t), value(v), line(l), colunm(c) {}
+
+  string getValue() {
+    stringstream ss;
+    ss << "L" << line << " ";
+    ss << "C" << colunm << " ";
+    ss << "T(" << type << ") ";
+    ss << "V: " << value << " ";
+    return ss.str();
+  }
 };
 
-int main(int argc, char *argv[]) { return 0; }
+vector<Token> tokenize(const string &input) {
+  vector<Token> tokens;
+  size_t pos = 0;
+  size_t line = 1;
+  size_t col = 1;
+
+  while (pos < input.length()) {
+    char c = input[pos];
+
+    switch (c) {
+    case '{':
+      tokens.emplace_back(TokenType::LEFT_BRACE, "{", line, col);
+      break;
+    case '}':
+      tokens.emplace_back(TokenType::RIGHT_BRACE, "}", line, col);
+      break;
+    case ':':
+      tokens.emplace_back(TokenType::COLON, ":", line, col);
+      break;
+    case ',':
+      tokens.emplace_back(TokenType::COMMA, ",", line, col);
+      break;
+    case '"': {
+      string str;
+      pos++;
+      col++;
+      while (pos < input.length() && input[pos] != '"') {
+        str += input[pos];
+        pos++;
+        col++;
+      }
+      tokens.emplace_back(TokenType::STRING, str, line, col);
+      break;
+    }
+    case ' ':
+    case '\t':
+      col++;
+      break;
+    case '\n':
+      line++;
+      col = 1;
+      break;
+    default:
+      if (isalpha(c)) {
+        string value;
+        while (pos < input.length() && std::isalnum(input[pos])) {
+          value += input[pos];
+          pos++;
+          col++;
+        }
+        pos--;
+        tokens.emplace_back(TokenType::STRING, value, line, col);
+      }
+    };
+    pos++;
+    col++;
+  }
+  return tokens;
+}
+
+int main(int argc, char *argv[]) {
+  string input = "{\"key\":\"value\",}";
+  auto tokens = tokenize(input);
+  for (auto token : tokens) {
+    cout << token.getValue() << endl;
+  }
+  return 0;
+}
